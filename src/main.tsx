ed315b8d0daa5c08d37be674d4ecfd14cd4882ac
arthurhenriqueg <85029788+arthurhenriqueg@users.noreply.github.com>
2025-05-03 06:58:19 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Habilita modo de produção (útil em algumas análises de performance)
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
