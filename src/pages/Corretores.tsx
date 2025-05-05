e5c9a345f230afb1eaf4c68bb0ba362dff80993b
arthurhenriqueg <85029788+arthurhenriqueg@users.noreply.github.com>
2025-05-05 11:23:24 -0300

    Update Corretores.tsx

--- src/pages/Corretores.tsx
+++ src/pages/Corretores.tsx
@@ -1,27 +1,26 @@
 import { memo } from 'react';
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
+import NavbarClean from "../components/NavbarClean";
+import HeroSectionClean from "../components/HeroSectionClean";
 import AboutSection from "../components/AboutSection";
 import FeaturesSection from "../components/FeaturesSection";
 import ImplementationSection from "../components/ImplementationSection";
 import LeisureSection from "../components/LeisureSection";
 import GallerySection from "../components/GallerySection";
 import LocationSection from "../components/LocationSection";
-import OpportunitySection from "../components/OpportunitySection";
+import OpportunitySectionClean from "../components/OpportunitySectionClean";
 
 const Corretores = () => {
   return (
     <div className="min-h-screen bg-beige">
-      <Navbar />
-      <HeroSection />
+      <NavbarClean />
+      <HeroSectionClean />
       <AboutSection />
       <FeaturesSection />
       <ImplementationSection />
       <LeisureSection />
       <GallerySection />
       <LocationSection />
-      <OpportunitySection />
-      {/* Footer e WhatsAppButton removidos */}
+      <OpportunitySectionClean />
     </div>
   );
 };
