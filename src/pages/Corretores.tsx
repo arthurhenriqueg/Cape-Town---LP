4975cba527cda1a2221ab846895cb190e0314f33
arthurhenriqueg <85029788+arthurhenriqueg@users.noreply.github.com>
2025-05-05 10:58:04 -0300

    Update Corretores.tsx

--- src/pages/Corretores.tsx
+++ src/pages/Corretores.tsx
@@ -1,1 +1,31 @@
+import { memo } from 'react';
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
+import FeaturesSection from "../components/FeaturesSection";
+import ImplementationSection from "../components/ImplementationSection";
+import LeisureSection from "../components/LeisureSection";
+import GallerySection from "../components/GallerySection";
+import LocationSection from "../components/LocationSection";
+import OpportunitySection from "../components/OpportunitySection";
+import ContactSection from "../components/ContactSection";
 
+const Corretores = () => {
+  return (
+    <div className="min-h-screen bg-beige">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <FeaturesSection />
+      <ImplementationSection />
+      <LeisureSection />
+      <GallerySection />
+      <LocationSection />
+      <OpportunitySection />
+      <ContactSection />
+      {/* Footer e WhatsAppButton removidos */}
+    </div>
+  );
+};
+
+export default memo(Corretores);
