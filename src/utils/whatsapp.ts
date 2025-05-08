1d70ef5745929fdb2dbaeab29c969629651f71dc
arthurhenriqueg <arthurhenriqueg585@gmail.com>
2025-05-08 09:10:44 -0300

    feat: alteração mensagem pronta wpp

--- src/utils/whatsapp.ts
+++ src/utils/whatsapp.ts
@@ -4,7 +4,7 @@
  * @param message Mensagem a ser enviada (opcional)
  * @returns URL formatada para WhatsApp
  */
-export const getWhatsAppLink = (message: string = "Gostei do Cape Town, posso saber mais?") => {
+export const getWhatsAppLink = (message: string = "Quero saber mais sobre o Cape Town") => {
   const phoneNumber = "5547992421044";
   const encodedMessage = encodeURIComponent(message);
   return `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
